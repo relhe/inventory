1795191e37a30fd37cba1cf89c40e7d880f76613
relhe <renel.lherisson@polymtl.ca>
2023-05-06 11:24:20 -0400

    set up new dependencies

---
+++ src/components/header/header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { HeaderWrapper } from './header.styled';
+
+const Header = () => <HeaderWrapper>Header Component</HeaderWrapper>;
+
+Header.propTypes = {};
+
+Header.defaultProps = {};
+
+export default Header;
