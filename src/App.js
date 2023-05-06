aa986fd997437705534c22cfce7fae762d87b0a4
relhe <renel.lherisson@polymtl.ca>
2023-05-06 17:53:26 -0400

    rendering card to DOM

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Register from './components/register/register';
 
 function App() {
     return (
         <div className="App">
-            <p>Hello friends!</p>
+            <Register />
         </div>
     );
 }
