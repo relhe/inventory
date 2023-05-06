2409b5e02d8e8548f8b01cd2cf450307ee1cd582
relhe <renel.lherisson@polymtl.ca>
2023-05-06 17:52:40 -0400

    added register component

---
+++ src/components/register/register.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Card from '../card/card';
+
+function Register(Props) {
+    return (
+        <div className="Register">
+            <p>
+                Register of {Props.count} {Props.carMake} {Props.CarModel} available in the current inventory!
+            </p>
+            <Card make="Ford" model="Mustang" year={1964} origin="USA" color="red" price={35000} />
+        </div>
+    );
+}
+
+Register.propTypes = {
+    count: PropTypes.number,
+    carMake: PropTypes.string,
+    carModel: PropTypes.string,
+};
+
+Register.defaultProps = {
+    count: 0,
+    carMake: 'Ford',
+    carModel: 'Mustang',
+};
+
+export default Register;
